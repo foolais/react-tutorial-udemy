8f863f053da42834eba0f900bed0f7b007ff5e8f
Wahyu Esya <wahyu.esya17@gmail.com>
2024-12-02 20:03:26 +0700

    add event listener n lifting state up

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,16 +1,29 @@
+/* eslint-disable react/prop-types */
 import styles from "./NewPost.module.css";
 
-function NewPost() {
+function NewPost(props) {
+  const { postChangeHandler } = props;
+
   return (
     <form className={styles.form}>
       <p className={styles.title}>New Post</p>
       <p>
         <label htmlFor="author">Your Name</label>
-        <input type="text" id="author" required />
+        <input
+          type="text"
+          id="author"
+          required
+          onChange={(event) => postChangeHandler(event, "author")}
+        />
       </p>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea
+          id="body"
+          required
+          rows={3}
+          onChange={(event) => postChangeHandler(event, "body")}
+        />
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,28 @@
 import Post from "./Post";
 import NewPost from "./NewPost";
 import styles from "./PostsList.module.css";
+import { useState } from "react";
 
 function PostList() {
+  const [author, setAuthor] = useState("The Impostor");
+  const [body, setBody] = useState("Something here...");
+
+  function postChangeHandler(event, type) {
+    const { value } = event.target;
+    if (type === "author") {
+      setAuthor(value && value.length > 0 ? value : "The Impostor");
+    } else if (type === "body") {
+      setBody(value && value.length > 0 ? value : "Something here..");
+    }
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost postChangeHandler={postChangeHandler} />
       <ul className={styles.postList}>
-        <Post author="Author" body="Body" />
+        <Post author="Author" body="Body Author" />
         <Post author="Co-Author" body="Body Co-Author" />
+        <Post author={author} body={body} />
       </ul>
     </>
   );
